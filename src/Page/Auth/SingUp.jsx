7326cbffbbe86d6e7c20d7bbc9a96fa38713df27
mahdiJihad <mahdijihad001@gmail.com>
2025-04-17 23:43:51 +0600

    Implement user registraction api

--- src/Page/Auth/SingUp.jsx
+++ src/Page/Auth/SingUp.jsx
@@ -3,15 +3,50 @@ import LogInImage from "../../assets/banner-img-1.png"
 import { Link } from 'react-router'
 import { FaRegUser } from 'react-icons/fa'
 import { IoBagHandle } from 'react-icons/io5'
+import BaseUrl from '../../Utils/BaseUrl/BaseUrl'
+import Swal from 'sweetalert2'
 
 
 const SingUp = () => {
 
-    const [role , setRole] = useState("candidate");
-    console.log(role);
+    const [role, setRole] = useState("candidate");
 
-    const FormData = (e) => {
+    const FormData = async (e) => {
         e.preventDefault();
+
+        const form = e.target;
+        const username = form.username.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        const user = {
+            username, email, password, role
+        };
+
+        const result = await fetch(`${BaseUrl()}/user/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        });
+
+        const finalResult = await result.json();
+
+        if (finalResult.status) {
+            Swal.fire({
+                title: finalResult.message,
+                icon: "success",
+                draggable: true
+            });
+        }else{
+            Swal.fire({
+                title: finalResult.message,
+                icon: "error",
+                draggable: true
+            });
+        }
+
     }
 
     return (
@@ -27,20 +62,20 @@ const SingUp = () => {
                 <div>
                     <form onSubmit={FormData} className="p-4 space-y-3">
                         <div className='grid grid-cols-2 gap-5'>
-                            <button onClick={() => setRole("candidate")} className={`flex items-center justify-center gap-2 py-4 text-[#1967d2] font-semibold text-[20px] ${role ==="candidate" ? "bg-[#34a853] text-[#ffff]" : "bg-[#e2eaf8]"} rounded-[8px] duration-500`}><FaRegUser /> Candidate</button>
-                            <button onClick={() => setRole("employer")} className={`flex items-center justify-center gap-2 py-4 text-[#1967d2] font-semibold text-[20px] ${role ==="employer" ? "bg-[#34a853] text-[#ffff]" : "bg-[#e2eaf8]"} rounded-[8px] duration-500`}><IoBagHandle /> Employer</button>
+                            <button type='button' onClick={() => setRole("candidate")} className={`flex items-center justify-center gap-2 py-4 text-[#1967d2] font-semibold text-[20px] ${role === "candidate" ? "bg-[#34a853] text-[#ffff]" : "bg-[#e2eaf8]"} rounded-[8px] duration-500`}><FaRegUser /> Candidate</button>
+                            <button type='button' onClick={() => setRole("employe")} className={`flex items-center justify-center gap-2 py-4 text-[#1967d2] font-semibold text-[20px] ${role === "employe" ? "bg-[#34a853] text-[#ffff]" : "bg-[#e2eaf8]"} rounded-[8px] duration-500`}><IoBagHandle /> Employer</button>
                         </div>
                         <div>
                             <label className='block text-[15px] font-bold leading-5 text-gray-500' htmlFor="">Username</label>
-                            <input className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="text" placeholder='Username' />
+                            <input name='username' className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="text" placeholder='Username' required />
                         </div>
                         <div>
                             <label className='block text-[15px] font-bold leading-5 text-gray-500' htmlFor="">Email</label>
-                            <input className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="email" placeholder='Email' />
+                            <input name='email' className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="email" placeholder='Email' required />
                         </div>
                         <div>
                             <label className='block text-[15px] font-bold leading-5 text-gray-500' htmlFor="">Password</label>
-                            <input className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="password" placeholder='Password' />
+                            <input name='password' className='w-full p-3.5 mt-2.5 bg-[#f0f5f7] rounded-md border-blue-300 focus:to-blue-300 outline-blue-300' type="password" placeholder='Password' required />
                         </div>
                         <div className='flex items-center justify-center mt-7'>
                             <button className='bg-[#1967d2] w-full py-3 rounded-md font-bold text-white text-xl hover:bg-[#6e86a8] duration-500'>Sing Up</button>
